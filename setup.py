6c8fe64404e56394ffba1e10c0536ee05f47148e
joosthoeks <joosthoeks@gmail.com>
2017-05-17 18:28:43 +0000

    modified

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='jhTAlib',
-    version='20170514.0',
+    version='20170517.0',
     description='Technical Analysis Library',
     keywords='Technical Analysis Library',
     url='https://github.com/joosthoeks/jhTAlib',
