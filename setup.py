0a00011eb9588cdf26beccb8a278409b853ee575
joosthoeks <joosthoeks@gmail.com>
2018-04-26 21:03:13 +0000

    modified

--- setup.py
+++ setup.py
@@ -1,31 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='jhTAlib',
-    version='20180112.0',
+    version='20180426.0',
     description='Technical Analysis Library',
     keywords='Technical Analysis Library',
     url='https://github.com/joosthoeks/jhTAlib',
     author='Joost Hoeks',
     author_email='joosthoeks@gmail.com',
     license='GNU',
-    packages=[
-        'jhtalib',
-        'jhtalib.behavioral_techniques',
-        'jhtalib.cycle_indicators',
-        'jhtalib.data',
-        'jhtalib.event_driven',
-        'jhtalib.experimental',
-        'jhtalib.math_functions',
-        'jhtalib.momentum_indicators',
-        'jhtalib.overlap_studies',
-        'jhtalib.pattern_recognition',
-        'jhtalib.price_transform',
-        'jhtalib.statistic_functions',
-        'jhtalib.volatility_indicators',
-        'jhtalib.volume_indicators',
-    ],
+    packages=find_packages(),
     install_requires=[
 #        'numpy',
 #        'pandas',
