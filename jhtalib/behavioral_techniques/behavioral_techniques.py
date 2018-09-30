94eddd0c254061fd54a06d898b5cb647be0e7568
joosthoeks <joosthoeks@gmail.com>
2018-09-30 18:24:29 +0000

    modified

--- jhtalib/behavioral_techniques/behavioral_techniques.py
+++ jhtalib/behavioral_techniques/behavioral_techniques.py
@@ -6,70 +6,77 @@ def ATH(df, price='High'):
     """
     All Time High
     """
-    ath_list = []
+    ath_dict = {'ath': [], 'ath_index': []}
     i = 0
     while i < len(df[price]):
         df_part_list = df[price][0:i+1]
         ath = max(df_part_list)
+        ath_dict['ath'].append(ath)
         ath_index = df_part_list.index(max(df_part_list))
-        ath_dict = {'ath': ath, 'ath_index': ath_index}
-        ath_list.append(ath_dict)
+        ath_dict['ath_index'].append(ath_index)
         i += 1
-    return ath_list
+    return ath_dict
 
 def LMC(df, price='Low'):
     """
     Last Major Correction
     """
-    lmc_list = []
-    ath_list = ATH(df)
+    lmc_dict = {'lmc': [], 'lmc_index': []}
+    ath_dict = ATH(df)
     i = 0
     while i < len(df[price]):
-        df_part_list = df[price][ath_list[i]['ath_index']:i+1]
+        df_part_list = df[price][ath_dict['ath_index'][i]:i+1]
         lmc = min(df_part_list)
+        lmc_dict['lmc'].append(lmc)
         lmc_index = df_part_list.index(min(df_part_list))
-        lmc_dict = {'lmc': lmc, 'lmc_index': lmc_index}
-        lmc_list.append(lmc_dict)
+        lmc_dict['lmc_index'].append(lmc_index)
         i += 1
-    return lmc_list
+    return lmc_dict
 
 def PP(df):
     """
     Pivot Point
     source: https://en.wikipedia.org/wiki/Pivot_point_(technical_analysis)
     """
-    pp_list = []
+    pp_dict = {'p': [], 'r1': [], 's1': [], 'r2': [], 's2': [], 'r3': [], 's3': []}
     i = 0
     while i < len(df['Close']):
         p = (df['High'][i] + df['Low'][i] + df['Close'][i]) / 3
+        pp_dict['p'].append(p)
         r1 = p + (p - df['Low'][i])
+        pp_dict['r1'].append(r1)
         s1 = p - (df['High'][i] - p)
+        pp_dict['s1'].append(s1)
         r2 = p + (df['High'][i] - df['Low'][i])
+        pp_dict['r2'].append(r2)
         s2 = p - (df['High'][i] - df['Low'][i])
+        pp_dict['s2'].append(s2)
         r3 = r1 + (df['High'][i] - df['Low'][i])
+        pp_dict['r3'].append(r3)
         s3 = s1 - (df['High'][i] - df['Low'][i])
-        pp_dict = {'p': p, 'r1': r1, 's1': s1, 'r2': r2, 's2': s2, 'r3': r3, 's3': s3}
-        pp_list.append(pp_dict)
+        pp_dict['s3'].append(s3)
         i += 1
-    return pp_list
+    return pp_dict
 
 def FIBOPR(df, price='Close'):
     """
     Fibonacci Price Retracements
     """
-    fibopr_list = []
+    fibopr_dict = {
+            '618': [], '-618': [],
+            '381': [], '-381': []
+            }
     i = 0
     p0618 = jhta.PHI() - 1
     p0381 = 1 - p0618
     while i < len(df[price]):
         p = df[price][i]
-        fibopr_dict = {
-            '618': p+(p*p0618), '-618': p-(p*p0618),
-            '381': p+(p*p0381), '-381': p-(p*p0381)
-            }
-        fibopr_list.append(fibopr_dict)
+        fibopr_dict['618'].append(p+(p*p0618))
+        fibopr_dict['-618'].append(p-(p*p0618))
+        fibopr_dict['381'].append(p+(p*p0381))
+        fibopr_dict['-381'].append(p-(p*p0381))
         i += 1
-    return fibopr_list
+    return fibopr_dict
 
 def FIBOTR(df, price='Close'):
     """
@@ -80,22 +87,34 @@ def GANNPR(df, price='Close'):
     """
     W. D. Gann Price Retracements
     """
-    gannpr_list = []
+    gannpr_dict = {
+            '1x8': [], '-1x8': [],
+            '1x4': [], '-1x4': [],
+            '1x2': [], '-1x2': [],
+            '1x1': [], '-1x1': [],
+            '2x1': [], '-2x1': [],
+            '4x1': [], '-4x1': [],
+            '8x1': [], '-8x1': []
+            }
     i = 0
     while i < len(df[price]):
         p = df[price][i]
-        gannpr_dict = {
-            '1x8': p+(p/8), '-1x8': p-(p/8),
-            '1x4': p+(p/8*2), '-1x4': p-(p/8*2),
-            '1x2': p+(p/8*3), '-1x2': p-(p/8*3),
-            '1x1': p+(p/8*4), '-1x1': p-(p/8*4),
-            '2x1': p+(p/8*5), '-2x1': p-(p/8*5),
-            '4x1': p+(p/8*6), '-4x1': p-(p/8*6),
-            '8x1': p+(p/8*7), '-8x1': p-(p/8*7)
-            }
-        gannpr_list.append(gannpr_dict)
+        gannpr_dict['1x8'].append(p+(p/8))
+        gannpr_dict['-1x8'].append(p-(p/8))
+        gannpr_dict['1x4'].append(p+(p/8*2))
+        gannpr_dict['-1x4'].append(p-(p/8*2))
+        gannpr_dict['1x2'].append(p+(p/8*3))
+        gannpr_dict['-1x2'].append(p-(p/8*3))
+        gannpr_dict['1x1'].append(p+(p/8*4))
+        gannpr_dict['-1x1'].append(p-(p/8*4))
+        gannpr_dict['2x1'].append(p+(p/8*5))
+        gannpr_dict['-2x1'].append(p-(p/8*5))
+        gannpr_dict['4x1'].append(p+(p/8*6))
+        gannpr_dict['-4x1'].append(p-(p/8*6))
+        gannpr_dict['8x1'].append(p+(p/8*7))
+        gannpr_dict['-8x1'].append(p-(p/8*7))
         i += 1
-    return gannpr_list
+    return gannpr_dict
 
 def GANNTR(df, price='Close'):
     """
