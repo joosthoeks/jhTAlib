f960948c5642c766e794da2203b8538108ed3995
joosthoeks <joosthoeks@gmail.com>
2018-11-05 15:45:04 +0000

    modified

--- jhtalib/overlap_studies/overlap_studies.py
+++ jhtalib/overlap_studies/overlap_studies.py
@@ -1,6 +1,25 @@
 import jhtalib as jhta
 
 
+def AVG(df, price='Close'):
+    """
+    Average
+    """
+    avg_list = []
+    start = None
+    i = 0
+    while i < len(df[price]):
+        if df[price][i] != df[price][i]:
+            avg = float('NaN')
+        else:
+            if start is None:
+                start = i
+            end = i + 1
+            avg = sum(df[price][start:end]) / (end - start)
+        avg_list.append(avg)
+        i += 1
+    return avg_list
+
 def BBANDS(df, n, f=2):
     """
     Bollinger Bands
