58f2772e2e82eae25aba33ff20e82008074848fb
joosthoeks <joosthoeks@gmail.com>
2019-01-03 19:35:32 +0000

    modified

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name='jhTAlib',
     version='20190103.0',
-    description='Technical Analysis Library',
-    keywords='Technical Analysis Library',
+    description='Technical Analysis Library Time-Series',
+    keywords='Technical Analysis Library Time-Series',
     url='https://github.com/joosthoeks/jhTAlib',
     author='Joost Hoeks',
     author_email='joosthoeks@gmail.com',
