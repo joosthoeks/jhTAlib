83c41c8288f57d6a26a2635b9e36c9f69304b37a
joosthoeks <joosthoeks@gmail.com>
2019-01-31 12:50:02 +0000

    modified

--- jhtalib/general/general.py
+++ jhtalib/general/general.py
@@ -6,16 +6,13 @@ def AVG(df, price='Close'):
     Average
     """
     avg_list = []
-    start = None
     i = 0
     while i < len(df[price]):
         if df[price][i] != df[price][i]:
             avg = float('NaN')
         else:
-            if start is None:
-                start = i
             end = i + 1
-            avg = sum(df[price][start:end]) / (end - start)
+            avg = sum(df[price][0:end]) / end
         avg_list.append(avg)
         i += 1
     return avg_list
@@ -25,16 +22,13 @@ def MED (df, price='Close'):
     MEDIAN
     """
     med_list = []
-    start = None
     i = 0
     while i < len(df[price]):
         if df[price][i] != df[price][i]:
             med = float('NaN')
         else:
-            if start is None:
-                start = i
             end = i + 1
-            med = (max(df[price][start:end]) + min(df[price][start:end])) / 2
+            med = (max(df[price][0:end]) + min(df[price][0:end])) / 2
         med_list.append(med)
         i += 1
     return med_list
