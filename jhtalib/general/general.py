7532ae99af76660385ef6cf034fe6bbf78654b1a
joosthoeks <joosthoeks@gmail.com>
2019-01-31 18:26:20 +0000

    modified

--- jhtalib/general/general.py
+++ jhtalib/general/general.py
@@ -1,3 +1,4 @@
+import math
 import jhtalib as jhta
 
 
@@ -41,11 +42,34 @@ def NORMALIZE(df, price_max='High', price_min='Low', price='Close'):
     normalize_list = []
     i = 0
     while i < len(df[price]):
-        end = i + 1
-        norm_max = max(df[price_max][0:end])
-        norm_min = min(df[price_min][0:end])
-        normalize = (df[price][i] - norm_min) / (norm_max - norm_min)
+        if i < 1:
+            normalize = float('NaN')
+        else:
+            end = i + 1
+            norm_max = max(df[price_max][0:end])
+            norm_min = min(df[price_min][0:end])
+            normalize = (df[price][i] - norm_min) / (norm_max - norm_min)
         normalize_list.append(normalize)
         i += 1
     return normalize_list
+
+def STANDARDIZE(df, price='Close'):
+    """
+    Standardize
+    source: https://machinelearningmastery.com/normalize-standardize-time-series-data-python/
+    """
+    standardize_list = []
+    i = 0
+    while i < len(df[price]):
+        if i < 1:
+            standardize = float('NaN')
+        else:
+            end = i + 1
+            x = df[price][0:end]
+            mean = AVG({'x': x}, 'x')[-1]
+            standard_deviation = jhta.STDEV({'x': x}, len(x), 'x')[-1]
+            standardize = (df[price][i] - mean) / standard_deviation
+        standardize_list.append(standardize)
+        i += 1
+    return standardize_list
 
