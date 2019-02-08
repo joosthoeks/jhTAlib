04a9cf2b72a08edd8f9707c3e51ff1301a0f616c
joosthoeks <joosthoeks@gmail.com>
2019-02-08 21:05:03 +0000

    modified

--- jhtalib/information/information.py
+++ jhtalib/information/information.py
@@ -1,3 +1,13 @@
 import jhtalib as jhta
 
 
+def INFO(df, columns=['datetime', 'Open', 'High', 'Low', 'Close', 'Volume']):
+    """
+    Print df Information
+    """
+
+def INFO_TRADES(profit_trades_list, loss_trades_list):
+    """
+    Print Trades Information
+    """
+
