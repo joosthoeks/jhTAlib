2befcfd00e8578268d13ae52e7a60164beb32a4e
joosthoeks <joosthoeks@gmail.com>
2019-02-10 21:49:27 +0000

    modified

--- jhtalib/information/information.py
+++ jhtalib/information/information.py
@@ -10,4 +10,12 @@ def INFO_TRADES(profit_trades_list, loss_trades_list):
     """
     Print Trades Information
     """
+    hr = jhta.HR(len(profit_trades_list), (len(profit_trades_list) + len(loss_trades_list)))
+    plr = jhta.PLR((sum(profit_trades_list) / len(profit_trades_list)), (sum(loss_trades_list) / len(loss_trades_list)))
+    ev = jhta.EV(hr, (sum(profit_trades_list) / len(profit_trades_list)), (sum(loss_trades_list) / len(loss_trades_list)))
+    por = jhta.POR(hr, plr)
+    print ('Hit Rate / Win Rate: %f' % hr)
+    print ('Profit/Loss Ratio: %f' % plr)
+    print ('Expected Value: %f' % ev)
+    print ('Probability of Ruin: %i' % por)
 
