29e3e83b70f3b7ba8ea2102debac46dae25f9fb7
joosthoeks <joosthoeks@gmail.com>
2019-02-21 19:57:19 +0000

    modified

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jhTAlib',
-    version='20190220.0',
+    version='20190221.0',
     description='Technical Analysis Library Time-Series',
     long_description='Technical Analysis Library Time-Series',
     keywords=['Technical', 'Analysis', 'Library', 'Time-Series'],
