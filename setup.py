e241f0951ef207d0e43b27d76d9cd2f5a097dafe
joosthoeks <joosthoeks@gmail.com>
2019-03-13 19:38:39 +0000

    modified

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(),
     classifiers=[
     'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: GNU General Public Licence v3 (GPLv3)',
+    'License :: OSI Approved :: GNU General Public Licence v3',
     'Operating System :: OS Independent',
     ],
 )
