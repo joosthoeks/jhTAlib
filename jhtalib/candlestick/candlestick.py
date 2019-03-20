03eb83abdd1ed45cb8b8127b567cefc830839228
joosthoeks <joosthoeks@gmail.com>
2019-03-20 21:23:05 +0000

    modified

--- jhtalib/candlestick/candlestick.py
+++ jhtalib/candlestick/candlestick.py
@@ -1,4 +1,15 @@
 import jhtalib as jhta
 
 
+def CDLBODYS(df):
+    """
+    Candle Body Size
+    """
+    cdl_list = []
+    i = 0
+    while i < len(df['Close']):
+        cdl = abs(df['Close'][i] - df['Open'][i])
+        cdl_list.append(cdl)
+        i += 1
+    return cdl_list
 
