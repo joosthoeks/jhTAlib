897bb5e4f2c03a2315d1b7a7c6e12198370d3dda
joosthoeks <joosthoeks@gmail.com>
2019-03-20 21:42:41 +0000

    modified

--- jhtalib/candlestick/candlestick.py
+++ jhtalib/candlestick/candlestick.py
@@ -25,3 +25,19 @@ def CDLWICKS(df):
         i += 1
     return cdl_list
 
+def CDLUPPSHAS(df):
+    """
+    Upper Shadow Size
+    """
+    cdl_list = []
+    i = 0
+    while i < len(df['Close']):
+        body = df['Close'][i] - df['Open'][i]
+        if body < 0:
+            cdl = df['High'][i] - df['Open'][i]
+        else:
+            cdl = df['High'][i] - df['Close'][i]
+        cdl_list.append(cdl)
+        i += 1
+    return cdl_list
+
