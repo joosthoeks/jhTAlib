ef2a02801bb145443b7db64775dfaa3f1479f94e
joosthoeks <joosthoeks@gmail.com>
2019-03-20 19:59:23 +0000

    modified

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 
-with open('docs/README.md', 'r') as f:
+with open('docs/README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 
 setup(
     name='jhTAlib',
-    version='20190320.1',
+    version='20190320.2',
     author='Joost Hoeks',
     author_email='joosthoeks@gmail.com',
     description='Technical Analysis Library Time-Series',
