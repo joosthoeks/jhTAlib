60e3a26d1836cb9d2072919467ce997f5742de9b
joosthoeks <joosthoeks@gmail.com>
2019-03-21 11:44:29 +0000

    modified

--- jhtalib/candlestick/candlestick.py
+++ jhtalib/candlestick/candlestick.py
@@ -8,7 +8,7 @@ def CDLBODYS(df):
     cdl_list = []
     i = 0
     while i < len(df['Close']):
-        cdl = abs(df['Close'][i] - df['Open'][i])
+        cdl = df['Close'][i] - df['Open'][i]
         cdl_list.append(cdl)
         i += 1
     return cdl_list
