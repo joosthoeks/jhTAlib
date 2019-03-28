548fb10cf062ecc5ac9ff2514439cc29c4baec73
joosthoeks <joosthoeks@gmail.com>
2019-03-28 19:30:08 +0000

    modified

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='jhTAlib',
-    version='20190326.0',
+    version='20190328.0',
     author='Joost Hoeks',
     author_email='joosthoeks@gmail.com',
     description='Technical Analysis Library Time-Series',
