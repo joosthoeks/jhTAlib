2a8342391af333865a67e4447d7abf0a920966df
joosthoeks <joosthoeks@gmail.com>
2019-06-10 09:57:32 +0000

    code md on one line

--- docs/pandoc.sh
+++ docs/pandoc.sh
@@ -4,7 +4,7 @@ pandoc -s -t epub --toc README.md -o docs/README.epub
 pandoc -s -t json --toc README.md -o docs/README.json
 pandoc -s -t odt --toc README.md -o docs/README.odt
 pandoc -s -t latex --toc README.md -o docs/README.pdf
-#pandoc -s -t rst --toc README.md -o docs/README.rst
-#pandoc -s -t rtf --toc README.md -o docs/README.rtf
+pandoc -s -t rst --toc README.md -o docs/README.rst
+pandoc -s -t rtf --toc README.md -o docs/README.rtf
 pandoc -s -t opendocument --toc README.md -o docs/README.xml
 
