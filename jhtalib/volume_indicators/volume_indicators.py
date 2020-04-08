46f52fd170d70adf916d47be26ca7afe617d70c9
joosthoeks <joosthoeks@gmail.com>
2020-04-08 19:41:00 +0000

    made more beautiful

--- jhtalib/volume_indicators/volume_indicators.py
+++ jhtalib/volume_indicators/volume_indicators.py
@@ -11,14 +11,12 @@ def AD(df, high='High', low='Low', close='Close', volume='Volume'):
     Chaikin A/D Line
     """
     ad_list = []
-    i = 0
-    while i < len(df[close]):
+    for i in range(len(df[close])):
         ad = 0
         if i > 0:
             clv = ((df[close][i] - df[low][i]) - (df[high][i] - df[close][i])) / (df[high][i] - df[low][i])
             ad = ad_list[i - 1] + clv * df[volume][i]
         ad_list.append(ad)
-        i += 1
     return ad_list
 
 def ADOSC(df):
@@ -30,21 +28,14 @@ def MFAI(df, high='High', low='Low', volume='Volume'):
     """
     Market Facilitation Index
     """
-    mfai_list = []
-    i = 0
-    while i < len(df[low]):
-        mfai = (df[high][i] - df[low][i]) / df[volume][i]
-        mfai_list.append(mfai)
-        i += 1
-    return mfai_list
+    return [(df[high][i] - df[low][i]) / df[volume][i] for i in range(len(df[low]))]
 
 def NVI(df, price='Close', volume='Volume'):
     """
     Negative Volume Index
     """
     nvi_list = []
-    i = 0
-    while i < len(df[price]):
+    for i in range(len(df[price])):
         nvi = 0
         if i > 0:
             if df[volume][i] < df[volume][i - 1]:
@@ -52,7 +43,6 @@ def NVI(df, price='Close', volume='Volume'):
             else:
                 nvi = nvi_list[i - 1]
         nvi_list.append(nvi)
-        i += 1
     return nvi_list
 
 def OBV(df, close='Close', volume='Volume'):
@@ -60,8 +50,7 @@ def OBV(df, close='Close', volume='Volume'):
     On Balance Volume
     """
     obv_list = []
-    i = 0
-    while i < len(df[close]):
+    for i in range(len(df[close])):
         obv = 0
         if i > 0:
             if df[close][i] > df[close][i - 1]:
@@ -71,7 +60,6 @@ def OBV(df, close='Close', volume='Volume'):
             else:
                 obv = obv_list[i - 1]
         obv_list.append(obv)
-        i += 1
     return obv_list
 
 def PVR(df, price='Close', volume='Volume'):
@@ -79,8 +67,7 @@ def PVR(df, price='Close', volume='Volume'):
     Price Volume Rank
     """
     pvr_list = []
-    i = 0
-    while i < len(df[price]):
+    for i in range(len(df[price])):
         if i < 1:
             pvr = float('NaN')
         else:
@@ -93,7 +80,6 @@ def PVR(df, price='Close', volume='Volume'):
             if df[price][i] < df[price][i - 1] and df[volume][i] > df[volume][i - 1]:
                 pvr = 4
         pvr_list.append(pvr)
-        i += 1
     return pvr_list
 
 def PVT(df, price='Close', volume='Volume'):
@@ -101,13 +87,11 @@ def PVT(df, price='Close', volume='Volume'):
     Price Volume Trend
     """
     pvt_list = []
-    i = 0
-    while i < len(df[price]):
+    for i in range(len(df[price])):
         pvt = 0
         if i > 0:
             pvt = pvt_list[i - 1] + df[volume][i] * (df[price][i] - df[price][i - 1]) / df[price][i - 1]
         pvt_list.append(pvt)
-        i += 1
     return pvt_list
 
 def PVI(df, price='Close', volume='Volume'):
@@ -115,8 +99,7 @@ def PVI(df, price='Close', volume='Volume'):
     Positive Volume Index
     """
     pvi_list = []
-    i = 0
-    while i < len(df[price]):
+    for i in range(len(df[price])):
         pvi = 0
         if i > 0:
             if df[volume][i] > df[volume][i - 1]:
@@ -124,7 +107,6 @@ def PVI(df, price='Close', volume='Volume'):
             else:
                 pvi = pvi_list[i - 1]
         pvi_list.append(pvi)
-        i += 1
     return pvi_list
 
 def VWAP(df, open='Open', high='High', low='Low', close='Close', volume='Volume'):
@@ -132,9 +114,8 @@ def VWAP(df, open='Open', high='High', low='Low', close='Close', volume='Volume'
     Volume Weighted Average Price
     """
     vwap_list = []
-    i = 0
     pvs = .0
-    while i < len(df[low]):
+    for i in range(len(df[close])):
         o = df[open][i]
         h = df[high][i]
         l = df[low][i]
@@ -144,6 +125,5 @@ def VWAP(df, open='Open', high='High', low='Low', close='Close', volume='Volume'
         pvs += (sum([o, h, l, c]) / 4) * v
         vwap = pvs / sum(df[volume][0:i + 1])
         vwap_list.append(vwap)
-        i += 1
     return vwap_list
 
