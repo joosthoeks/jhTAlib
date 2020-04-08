230d7abd6ce280e36153b63dce1a33aeddd6c37c
joosthoeks <joosthoeks@gmail.com>
2020-04-08 17:59:42 +0000

    made more beautiful

--- jhtalib/price_transform/price_transform.py
+++ jhtalib/price_transform/price_transform.py
@@ -11,35 +11,25 @@ def AVGPRICE(df, open='Open', high='High', low='Low', close='Close'):
     Average Price
     """
     avgprice_list = []
-    i = 0
-    while i < len(df[close]):
+    for i in range(len(df[close])):
         avgprice = (df[open][i] + df[high][i] + df[low][i] + df[close][i]) / 4
         avgprice_list.append(avgprice)
-        i += 1
     return avgprice_list
 
 def MEDPRICE(df, high='High', low='Low'):
     """
     Median Price
     """
-    medprice_list = []
-    i = 0
-    while i < len(df[low]):
-        medprice = (df[high][i] + df[low][i]) / 2
-        medprice_list.append(medprice)
-        i += 1
-    return medprice_list
+    return [(df[high][i] + df[low][i]) / 2 for i in range(len(df[low]))]
 
 def TYPPRICE(df, high='High', low='Low', close='Close'):
     """
     Typical Price
     """
     typprice_list = []
-    i = 0
-    while i < len(df[close]):
+    for i in range(len(df[close])):
         typprice = (df[high][i] + df[low][i] + df[close][i]) / 3
         typprice_list.append(typprice)
-        i += 1
     return typprice_list
 
 def WCLPRICE(df, high='High', low='Low', close='Close'):
@@ -47,10 +37,8 @@ def WCLPRICE(df, high='High', low='Low', close='Close'):
     Weighted Close Price
     """
     wclprice_list = []
-    i = 0
-    while i < len(df[close]):
+    for i in range(len(df[close])):
         wclprice = (df[high][i] + df[low][i] + (df[close][i] * 2)) / 4
         wclprice_list.append(wclprice)
-        i += 1
     return wclprice_list
 
