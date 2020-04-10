486f9d5d13522a3c093e2b45553923972cc0ae7c
joosthoeks <joosthoeks@gmail.com>
2020-04-10 14:03:51 +0000

    reorder defs

--- jhtalib/candlestick/candlestick.py
+++ jhtalib/candlestick/candlestick.py
@@ -7,61 +7,6 @@
 import jhtalib as jhta
 
 
-def CDLBODYS(df, open='Open', close='Close'):
-    """
-    Candle Body Size
-    Returns: list of floats = jhta.CDLBODYS(df, open='Open', close='Close')
-    Source: https://www.tradeciety.com/understand-candlesticks-patterns/
-    """
-    return [df[close][i] - df[open][i] for i in range(len(df[close]))]
-
-def CDLWICKS(df, high='High', low='Low'):
-    """
-    Candle Wick Size
-    Returns: list of floats = jhta.CDLWICKS(df, high='High', low='Low')
-    Source: https://www.tradeciety.com/understand-candlesticks-patterns/
-    """
-    return [df[high][i] - df[low][i] for i in range(len(df[low]))]
-
-def CDLUPPSHAS(df, open='Open', high='High', close='Close'):
-    """
-    Candle Upper Shadow Size
-    Returns: list of floats = jhta.CDLUPPSHAS(df, open='Open', high='High', close='Close')
-    Source: https://www.tradeciety.com/understand-candlesticks-patterns/
-    """
-    cdl_list = []
-    for i in range(len(df[close])):
-        body = df[close][i] - df[open][i]
-        if body < 0:
-            cdl = df[high][i] - df[open][i]
-        else:
-            cdl = df[high][i] - df[close][i]
-        cdl_list.append(cdl)
-    return cdl_list
-
-def CDLLOWSHAS(df, open='Open', low='Low', close='Close'):
-    """
-    Candle Lower Shadow Size
-    Returns: list of floats = jhta.CDLLOWSHAS(df, open='Open', low='Low', close='Close')
-    Source: https://www.tradeciety.com/understand-candlesticks-patterns/
-    """
-    cdl_list = []
-    for i in range(len(df[close])):
-        body = df[close][i] - df[open][i]
-        if body < 0:
-            cdl = df[close][i] - df[low][i]
-        else:
-            cdl = df[open][i] - df[low][i]
-        cdl_list.append(cdl)
-    return cdl_list
-
-def CDLBODYP(df, open='Open', close='Close'):
-    """
-    Candle Body Percent
-    Returns: list of floats = jhta.CDLBODYP(df, open='Open', close='Close')
-    """
-    return [(df[close][i] - df[open][i]) / df[open][i] for i in range(len(df[close]))]
-
 def CDLBODYM(df, n, open='Open', close='Close'):
     """
     Candle Body Momentum
@@ -87,6 +32,61 @@ def CDLBODYM(df, n, open='Open', close='Close'):
         cdl_list.append(cdl)
     return cdl_list
 
+def CDLBODYP(df, open='Open', close='Close'):
+    """
+    Candle Body Percent
+    Returns: list of floats = jhta.CDLBODYP(df, open='Open', close='Close')
+    """
+    return [(df[close][i] - df[open][i]) / df[open][i] for i in range(len(df[close]))]
+
+def CDLBODYS(df, open='Open', close='Close'):
+    """
+    Candle Body Size
+    Returns: list of floats = jhta.CDLBODYS(df, open='Open', close='Close')
+    Source: https://www.tradeciety.com/understand-candlesticks-patterns/
+    """
+    return [df[close][i] - df[open][i] for i in range(len(df[close]))]
+
+def CDLLOWSHAS(df, open='Open', low='Low', close='Close'):
+    """
+    Candle Lower Shadow Size
+    Returns: list of floats = jhta.CDLLOWSHAS(df, open='Open', low='Low', close='Close')
+    Source: https://www.tradeciety.com/understand-candlesticks-patterns/
+    """
+    cdl_list = []
+    for i in range(len(df[close])):
+        body = df[close][i] - df[open][i]
+        if body < 0:
+            cdl = df[close][i] - df[low][i]
+        else:
+            cdl = df[open][i] - df[low][i]
+        cdl_list.append(cdl)
+    return cdl_list
+
+def CDLUPPSHAS(df, open='Open', high='High', close='Close'):
+    """
+    Candle Upper Shadow Size
+    Returns: list of floats = jhta.CDLUPPSHAS(df, open='Open', high='High', close='Close')
+    Source: https://www.tradeciety.com/understand-candlesticks-patterns/
+    """
+    cdl_list = []
+    for i in range(len(df[close])):
+        body = df[close][i] - df[open][i]
+        if body < 0:
+            cdl = df[high][i] - df[open][i]
+        else:
+            cdl = df[high][i] - df[close][i]
+        cdl_list.append(cdl)
+    return cdl_list
+
+def CDLWICKS(df, high='High', low='Low'):
+    """
+    Candle Wick Size
+    Returns: list of floats = jhta.CDLWICKS(df, high='High', low='Low')
+    Source: https://www.tradeciety.com/understand-candlesticks-patterns/
+    """
+    return [df[high][i] - df[low][i] for i in range(len(df[low]))]
+
 def GAP(df, high='High', low='Low'):
     """
     Gap
@@ -104,43 +104,6 @@ def GAP(df, high='High', low='Low'):
                 gap = df[high][i] - df[low][i - 1]
         gap_list.append(gap)
     return gap_list
-
-def QSTICK(df, n, open='Open', close='Close'):
-    """
-    Qstick
-    Returns: list of floats = jhta.QSTICK(df, n, open='Open', close='Close')
-    Source: https://www.fmlabs.com/reference/default.htm?url=Qstick.htm
-    """
-    qstick_list = []
-    for i in range(len(df[close])):
-        if i + 1 < n:
-            qstick = float('NaN')
-        else:
-            start = i + 1 - n
-            end = i + 1
-            qstick = sum(jhta.CDLBODYS(df, open, close)[start:end]) / n
-        qstick_list.append(qstick)
-    return qstick_list
-
-def SHADOWT(df, n, open='Open', high='High', low='Low', close='Close'):
-    """
-    Shadow Trends
-    Returns: dict of lists of floats = jhta.SHADOWT(df, n, open='Open', high='High', low='Low', close='Close')
-    Source: book: The New Technical Trader
-    """
-    shadowt_dict = {'upper': [], 'lower': []}
-    for i in range(len(df[close])):
-        if i + 1 < n:
-            upper = float('NaN')
-            lower = float('NaN')
-        else:
-            start = i + 1 - n
-            end = i + 1
-            upper = sum(jhta.CDLUPPSHAS(df, open, high, close)[start:end]) / n
-            lower = sum(jhta.CDLLOWSHAS(df, open, low, close)[start:end]) / n
-        shadowt_dict['upper'].append(upper)
-        shadowt_dict['lower'].append(lower)
-    return shadowt_dict
 
 def IMI(df, open='Open', close='Close'):
     """
@@ -191,3 +154,41 @@ def OUTSBAR(df, high='High', low='Low'):
                 outsbar = 1
         outsbar_list.append(outsbar)
     return outsbar_list
+
+def QSTICK(df, n, open='Open', close='Close'):
+    """
+    Qstick
+    Returns: list of floats = jhta.QSTICK(df, n, open='Open', close='Close')
+    Source: https://www.fmlabs.com/reference/default.htm?url=Qstick.htm
+    """
+    qstick_list = []
+    for i in range(len(df[close])):
+        if i + 1 < n:
+            qstick = float('NaN')
+        else:
+            start = i + 1 - n
+            end = i + 1
+            qstick = sum(jhta.CDLBODYS(df, open, close)[start:end]) / n
+        qstick_list.append(qstick)
+    return qstick_list
+
+def SHADOWT(df, n, open='Open', high='High', low='Low', close='Close'):
+    """
+    Shadow Trends
+    Returns: dict of lists of floats = jhta.SHADOWT(df, n, open='Open', high='High', low='Low', close='Close')
+    Source: book: The New Technical Trader
+    """
+    shadowt_dict = {'upper': [], 'lower': []}
+    for i in range(len(df[close])):
+        if i + 1 < n:
+            upper = float('NaN')
+            lower = float('NaN')
+        else:
+            start = i + 1 - n
+            end = i + 1
+            upper = sum(jhta.CDLUPPSHAS(df, open, high, close)[start:end]) / n
+            lower = sum(jhta.CDLLOWSHAS(df, open, low, close)[start:end]) / n
+        shadowt_dict['upper'].append(upper)
+        shadowt_dict['lower'].append(lower)
+    return shadowt_dict
+
