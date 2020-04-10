ad7da1bbb8b0d2e7b2e629cd616d02d70b00b28a
joosthoeks <joosthoeks@gmail.com>
2020-04-10 14:11:28 +0000

    reorder defs

--- jhtalib/event_driven/event_driven.py
+++ jhtalib/event_driven/event_driven.py
@@ -25,37 +25,6 @@ def ASI(df, L, open='Open', high='High', low='Low', close='Close'):
             asi_list.append(asi)
     return asi_list
 
-def SI(df, L, open='Open', high='High', low='Low', close='Close'):
-    """
-    Swing Index (J. Welles Wilder)
-    Returns: list of floats = jhta.SI(df, L, open='Open', high='High', low='Low', close='Close')
-    Source: book: New Concepts in Technical Trading Systems
-    """
-    si_list = []
-    for i in range(len(df[close])):
-        if i < 1:
-            si = float('NaN')
-        else:
-            N = (df[close][i] - df[close][i - 1]) + (.5 * (df[close][i] - df[open][i])) + (.25 * (df[close][i - 1] - df[open][i - 1]))
-            R1 = df[high][i] - df[close][i - 1]
-            R2 = df[low][i] - df[close][i - 1]
-            R3 = df[high][i] - df[low][i]
-            if R1 > R2 and R1 > R3:
-                R = (df[high][i] - df[close][i - 1]) - (.5 * (df[low][i] - df[close][i - 1])) + (.25 * (df[close][i - 1] - df[open][i - 1]))
-            if R2 > R1 and R2 > R3:
-                R = (df[low][i] - df[close][i - 1]) - (.5 * (df[high][i] - df[close][i - 1])) + (.25 * (df[close][i - 1] - df[open][i - 1]))
-            if R3 > R1 and R3 > R2:
-                R = (df[high][i] - df[low][i]) + (.25 * (df[close][i - 1] - df[open][i - 1]))
-            K1 = df[high][i] - df[close][i - 1]
-            K2 = df[low][i] - df[close][i - 1]
-            if K1 > K2:
-                K = K1
-            else:
-                K = K2
-            si = 50 * (N / R) * (K / L)
-        si_list.append(si)
-    return si_list
-
 def SAVGP(df, open='Open', high='High', low='Low', close='Close'):
     """
     Swing Average Price - previous Average Price
@@ -107,6 +76,37 @@ def SCOS(df, open='Open', close='Close'):
         scos = scos + sco_list[i]
         scos_list.append(scos)
     return scos_list
+
+def SI(df, L, open='Open', high='High', low='Low', close='Close'):
+    """
+    Swing Index (J. Welles Wilder)
+    Returns: list of floats = jhta.SI(df, L, open='Open', high='High', low='Low', close='Close')
+    Source: book: New Concepts in Technical Trading Systems
+    """
+    si_list = []
+    for i in range(len(df[close])):
+        if i < 1:
+            si = float('NaN')
+        else:
+            N = (df[close][i] - df[close][i - 1]) + (.5 * (df[close][i] - df[open][i])) + (.25 * (df[close][i - 1] - df[open][i - 1]))
+            R1 = df[high][i] - df[close][i - 1]
+            R2 = df[low][i] - df[close][i - 1]
+            R3 = df[high][i] - df[low][i]
+            if R1 > R2 and R1 > R3:
+                R = (df[high][i] - df[close][i - 1]) - (.5 * (df[low][i] - df[close][i - 1])) + (.25 * (df[close][i - 1] - df[open][i - 1]))
+            if R2 > R1 and R2 > R3:
+                R = (df[low][i] - df[close][i - 1]) - (.5 * (df[high][i] - df[close][i - 1])) + (.25 * (df[close][i - 1] - df[open][i - 1]))
+            if R3 > R1 and R3 > R2:
+                R = (df[high][i] - df[low][i]) + (.25 * (df[close][i - 1] - df[open][i - 1]))
+            K1 = df[high][i] - df[close][i - 1]
+            K2 = df[low][i] - df[close][i - 1]
+            if K1 > K2:
+                K = K1
+            else:
+                K = K2
+            si = 50 * (N / R) * (K / L)
+        si_list.append(si)
+    return si_list
 
 def SMEDP(df, high='High', low='Low'):
     """
