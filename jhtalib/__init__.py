9e1ba80bdd209f1d4ea21bba4e85d0e6524f7e66
joosthoeks <joosthoeks@gmail.com>
2020-12-28 18:20:21 +0000

    updated

--- jhtalib/__init__.py
+++ jhtalib/__init__.py
@@ -3,7 +3,7 @@
 
 # Set Global Attributes:
 __name__ = 'jhTAlib'
-__version__ = '20200412.0'
+__version__ = '20201228.0'
 __description__ = 'Technical Analysis Library Time-Series'
 __url__ = 'https://github.com/joosthoeks/jhTAlib'
 __author__ = 'Joost Hoeks'
