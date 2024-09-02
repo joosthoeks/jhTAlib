a0a7198a0dc65c9c7eab26e136d7642d1506164e
joosthoeks <joosthoeks@gmail.com>
2024-09-02 12:36:13 +0000

    added def get_leverage

--- jhtalib/derivatives/derivatives.py
+++ jhtalib/derivatives/derivatives.py
@@ -7,3 +7,9 @@
 import jhtalib as jhta
 
 
+def get_leverage(total_exposure, total_equity):
+    """
+    Leverage
+    Returns: float = jhta.get_leverage(total_exposure, total_equity)
+    """
+    return total_exposure / total_equity
