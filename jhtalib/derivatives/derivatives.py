60a5468ac34590ba7864b772c7c8ad7b26748606
joosthoeks <joosthoeks@gmail.com>
2024-09-02 12:43:07 +0000

    added def get_ltv

--- jhtalib/derivatives/derivatives.py
+++ jhtalib/derivatives/derivatives.py
@@ -13,3 +13,11 @@ def get_leverage(total_exposure, total_equity):
     Returns: float = jhta.get_leverage(total_exposure, total_equity)
     """
     return total_exposure / total_equity
+
+def get_ltv(total_exposure, total_equity):
+    """
+    Loan to Value
+    Returns: float = jhta.get_ltv(total_exposure, total_equity)
+    """
+    return (total_exposure - total_equity) / total_exposure
+
