07b9e7b08249f882d995e8e0ddc4d7780683ba1e
joosthoeks <joosthoeks@gmail.com>
2025-06-24 17:51:22 +0000

    added def get_elp_long

--- jhtalib/derivatives/derivatives.py
+++ jhtalib/derivatives/derivatives.py
@@ -7,6 +7,14 @@
 import jhtalib as jhta
 
 
+def get_elp_long(entry_price, leverage):
+    """
+    Estimated Liquidation Price Long
+    Returns: float = jhta.get_elp_long(entry_price, leverage)
+    Source: https://leverage.trading/liquidation-price-calculator/
+    """
+    return entry_price - (entry_price / leverage)
+
 def get_leverage(total_exposure, total_equity):
     """
     Leverage
